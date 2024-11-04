41ef53ce9434e8c779ea52764a78796830d06c69
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-11-04 21:46:17 +0100

    restore old index + update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/max-char-length-calculator/', // Replace 'my-vue-app' with your actual repository name
   plugins: [
     vue(),
   ],
