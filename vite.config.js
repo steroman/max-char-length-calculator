21ea04b7978424d6aa0b8d2aa521c8ab247e6262
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-11-04 17:49:06 +0100

    new vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-  base: './',
+  base: '/max-char-length-calculator/',
   plugins: [vue()],
   resolve: {
     alias: {
