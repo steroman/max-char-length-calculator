0bb3786e6fcaccbb2382d816b925e0c6ff908ae0
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-11-10 23:14:57 +0100

    add new variable for analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, onValue, increment, set, get } from 'firebase/database';
 
 const firebaseConfig = {
@@ -8,10 +9,12 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const database = getDatabase(app);
 const usageCountRef = ref(database, 'usageCount');
 
